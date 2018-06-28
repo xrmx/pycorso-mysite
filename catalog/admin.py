2b0a234d566ae796f1f8034c21b43fc6e47bd3f5
Riccardo Magliocchetti <riccardo.magliocchetti@gmail.com>
2018-06-28 08:46:26 +0000

    catalog: register models in admin

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Book, Author, BookInstance, Genre
 
-# Register your models here.
+
+admin.site.register(Book)
+admin.site.register(Author)
+admin.site.register(BookInstance)
+admin.site.register(Genre)
