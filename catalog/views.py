177367132c0586b46c101674747f67c2e369c289
Riccardo Magliocchetti <riccardo.magliocchetti@gmail.com>
2018-07-05 08:49:49 +0000

    Add catalog index view

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Book, BookInstance, Author, Genre
+
+
+def index(request):
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    num_authors = Author.objects.count()
+    num_genres = Genre.objects.count()
+    num_libro_books = Book.objects.filter(title__icontains='libro').count()
+
+    return render(
+        request,
+        'index.html',
+        context={
+            'num_books': num_books,
+            'num_instances': num_instances,
+            'num_instances_available': num_instances_available,
+            'num_authors': num_authors,
+            'num_genres': num_genres,
+            'num_libro_books': num_libro_books,
+        }
+    )
